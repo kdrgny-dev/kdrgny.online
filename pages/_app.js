c197b8520d7853329a9e68ed57dc3ec5e54c9d77
Kadir Günay <42644031+kdrgny-dev@users.noreply.github.com>
2022-11-19 22:02:40 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,28 @@
+import Script from 'next/script'
 import { ThemeProvider } from 'next-themes'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-YGP5NEHQZL`} />
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-YGP5NEHQZL');
+        `,
+        }}
+      />
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+
   )
 }
 
