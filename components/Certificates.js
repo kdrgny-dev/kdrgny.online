6f437494b41b5a3fa2012d3838b563f4f7fec715
Kadir Günay <42644031+kdrgny-dev@users.noreply.github.com>
2022-11-19 15:25:10 +0300

    path changed

--- components/Certificates.js
+++ components/Certificates.js
@@ -7,12 +7,14 @@ export default function Certificates({ data }) {
   return (
     <section>
       <Heading level={4}>CERTIFICATES</Heading>
-      <div className="grid xl:grid-cols-4 md:grid-cols-4 grid-cols-2 gap-2">
+      <div className="grid xl:grid-cols-4 md:grid-cols-4 grid-cols-2 gap-2 xl:gap-4">
         {data?.map((item, index) => (
           <Link key={index} href={item.url}>
             <a target="_blank" className='flex flex-col items-center group hover:shadow-lg dark:hover:shadow-gray-500 transition duration-200 ease-linear rounded'>
               <div className='relative w-32 h-20 xl:w-36 xl:h-24 overflow-hidden rounded'>
-                <Image src={item?.image} alt={item.title} layout="fill" className='group-hover:scale-105 transition duration-200 ease-linear' />
+                <Image src={item?.image} alt={item.title} layout="responsive" width={144} height={96} className='group-hover:scale-105 transition duration-200 ease-linear' loading="lazy" placeholder='blur'
+                  blurDataURL={`${item?.image}&w=16&q=1`}
+                />
               </div>
               <div className="p-2 text-xs text-gray-700 font-semibold text-center dark:text-gray-400 dark:group-hover:text-gray-100 transition duration-200 ease-linear">
                 {item.title}
